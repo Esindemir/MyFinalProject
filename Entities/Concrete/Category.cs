d1c4545fc8e02b1d2375d7ebd37bda71d33eb722
esndmr <esndmr@gmail.com>
2021-02-22 22:47:07 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Category:IEntity 
+    {
+        //Çıplak Classs Kalmasın 
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
