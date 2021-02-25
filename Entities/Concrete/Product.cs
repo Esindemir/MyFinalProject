5c048d0f40c72680f30d879c4c51ce2778f15115
esndmr <esndmr@gmail.com>
2021-02-25 09:09:00 +0300

    22.02.2021 Ders Sonu A

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -5,12 +5,11 @@ using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Product:IEntity 
+    public class Product : IEntity
     {
         public int ProductId { get; set; }
         public int CategoryId { get; set; }
         public string ProductName { get; set; }
-        public int MyProperty { get; set; }
         public short UnitsInStock { get; set; }
         public decimal UnitPrice { get; set; } 
 
